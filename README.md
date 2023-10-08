1ce6b0d95c12ecb86feb01392f353de537feb652
Koseng <26673978+Koseng@users.noreply.github.com>
2023-10-08 15:52:46 +0200

    Update

--- README.md
+++ README.md
@@ -6,34 +6,47 @@ For using Linux USB "On-The-Go" (OTG)  with the Raspberry Pi Zero W, a different
 
 For OTG mode there are different [gadget drivers](http://www.linux-usb.org/gadget/). A Raspberry Pi Zero W can behave like an USB stick with the [g_mass_storage](https://www.kernel.org/doc/Documentation/usb/mass-storage.txt) driver. A more complex alternative is using libcomposite with configfs.
 
-### Loading the driver
+### Load the driver
 
-#### config.txt
-In /boot/config.txt comment out otg_mode and add dtoverlay=dwc2
+:information_source: In `/boot/config.txt` comment out `otg_mode` and add `dtoverlay=dwc2`
 
 ```
-# Comment out
-# otg_mode=1
-dtoverlay=dwc2
+sudo sed -i '/^otg_mode=1/s/^/#/' /boot/config.txt
+sudo sh -c "echo 'dtoverlay=dwc2' >> /boot/config.txt"
 ```
 
-#### cmdline.txt
-After rootwait in /boot/cmdline.txt add as one line:
+:information_source: In `/etc/modules` add `dwc2`:
 
-```modules-load=dwc2,g_mass_storage g_mass_storage.file=/piusb.bin g_mass_storage.stall=0 g_mass_storage.removable=y g_mass_storage.ro=0 g_mass_storage.iSerialNumber=1234567890```
+```
+sudo sh -c "echo 'dwc2' >> /etc/modules"
+```
 
-### Creating a 2GB USB share
+### Create the 2GB USB share
 
-The content of the USB share is stored in a file called piusb.bin.
+:information_source: First install packages for exfat file system. Then create and format usb image file piusb.bin. Finally prepare mounting.
 
  ```
 sudo apt-get install exfat-fuse
 sudo apt-get install exfat-utils
 sudo dd bs=1M if=/dev/zero of=/piusb.bin count=2048 # create file
 sudo mkfs.exfat /piusb.bin # format
-
 sudo mkdir /mnt/usb_share
-sudo nano /etc/fstab → /piusb.bin /mnt/usb_share exfat noauto,nofail,users,umask=000 0 2
- ```
+```
+:information_source: In `/etc/fstab` add `/piusb.bin /mnt/usb_share exfat noauto,nofail,users,umask=000 0 2`
+```
+sudo sh -c "echo '/piusb.bin /mnt/usb_share exfat noauto,nofail,users,umask=000 0 2' >> /etc/fstab"
+```
+
+### Activate USB mass storage
+Create system service to activate USB mass storage at the very end of the boot process. Therefore setting the type to idle is important. 
+
+Copy `usb-storage.service` to `/etc/systemd/system/` then
+```
+sudo chmod 644 /etc/systemd/system/usb-storage.service
+sudo systemctl enable usb-storage.service
+```
+
+
+ 
 
 
--- src/get_new_usb_files.py
+++ src/get_new_usb_files.py
@@ -1,31 +1,60 @@
 import os, subprocess, shutil, time
+from pathlib import Path
 
-def get_files_since_point_in_time(rootdir, pointInTime):
+def get_files_since_point_in_time(rootdir, pointInTime, newestDirCount):
     allFiles = []
+    directories = []
     for item in os.scandir(rootdir):
         if item.is_file() and (item.stat().st_mtime > pointInTime):          
-            allFiles.append(item.path)          
-        elif item.is_dir():             
-            if (item.stat().st_atime > pointInTime): # continue in folder if it was accessed
-                get_files_since_point_in_time(item, pointInTime)
+            allFiles.append(item.path)
+        elif item.is_dir():
+            directories.append(item)                    
+    # Continue for the newestDirCount newest folders in current directory
+    directories.sort(key=lambda d: d.stat().st_mtime, reverse=True)
+    for i in range(newestDirCount):
+        if len(directories) > i:
+            allFiles.extend(get_files_since_point_in_time(directories[i], pointInTime, newestDirCount))
     return allFiles
 
+
+def copy_file_with_directory_structure(sourceBasePath, sourceFile, destBasePath):
+    relativePath = os.path.relpath(sourceFile, sourceBasePath)   
+    dstPath =  os.path.join(destBasePath, os.path.dirname(relativePath))
+    os.makedirs(dstPath, exist_ok=True)
+    shutil.copy2(sourceFile, dstPath)
+
+def aquire_lock(lockFile):
+    if os.path.isfile(lockFile):
+        time.sleep(0.2)
+    else:
+        Path(lockFile).touch()
+
+def release_lock(lockFile):
+    if os.path.isfile(lockFile):
+        os.remove(lockFile) 
+
 # WORK IN PROGRESS
-fileToCheck = "c:/temp/test.txt"
-sourceDir = '/mnt/usb_share'
-destinationDir = '~/transfer'
+fileToCheck = "c:/temp/test.txt"  # /piusb.bin
+sourceDir = 'c:/temp/'
+destinationDir = 'c:/dest/'
+# sourceDir = '/mnt/usb_share'
+# destinationDir = '~/transfer'
 lastModifyTime = os.stat(fileToCheck).st_mtime
+lockFile = os.path.join(destinationDir, 'lock')
 
 while (True):
     currentModifyTime = os.stat(fileToCheck).st_mtime
     if (currentModifyTime > lastModifyTime):        
-        subprocess.call(['sudo', 'modprobe', '-r', 'g_mass_storage'])        
-        subprocess.call(['sudo', 'mount', '-a'])
+        aquire_lock(lockFile)
+        # subprocess.call(['sudo', 'modprobe', '-r', 'g_mass_storage'])        
+        # subprocess.call(['sudo', 'mount', sourceDir])
         time.sleep(1)        
-        newFiles = get_files_since_point_in_time(sourceDir, lastModifyTime)
-        paths = [shutil.copy2(file, destinationDir) for file in newFiles]        
-        subprocess.call(['sudo', 'umount', '-f', sourceDir]) # -f force
-        subprocess.call(['sudo', 'modprobe', 'g_mass_storage', 'file=/piusb.bin', 'stall=0', 'removable=y', 'iSerialNumber=1234567890'])      
+        newFiles = get_files_since_point_in_time(sourceDir, lastModifyTime, newestDirCount=2)       
+        for file in newFiles:
+            copy_file_with_directory_structure(sourceDir, file, destinationDir)      
+        # subprocess.call(['sudo', 'umount', '-f', sourceDir]) # -f force
+        # subprocess.call(['sudo', 'modprobe', 'g_mass_storage', 'file=/piusb.bin', 'stall=0', 'removable=y', 'iSerialNumber=1234567890'])      
+        release_lock(lockFile)
         lastModifyTime = currentModifyTime
     else:
         time.sleep(5)
